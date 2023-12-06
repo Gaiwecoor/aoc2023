3510dc779e811489b1814098b99e63d6de910d93
Kevin Dixon <kevin.dixon@accura.healthcare>
2023-12-06 10:59:59 -0600

    Basic single-run timing

--- run.js
+++ run.js
@@ -5,11 +5,18 @@ const input = fs.readFileSync(`./input/input${day}.txt`, "utf8");
 
 const { setup, part1, part2 } = require(`./solutions/day${day.padStart(2, "0")}`);
 
-const parsed = setup(input);
-
 console.log("DAY", day);
-const result1 = part1(parsed);
-console.log("Part 1:", result1);
 
+console.time("Setup");
+const parsed = setup(input);
+console.timeEnd("Setup");
+
+console.time("Part 1");
+const result1 = part1(parsed);
+console.timeEnd("Part 1");
+console.log("Part 1 Result:", result1);
+
+console.time("Part 2");
 const result2 = part2(parsed, result1);
-console.log("Part 2:", result2);
+console.timeEnd("Part 2");
+console.log("Part 2 Result:", result2);
